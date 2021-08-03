2dd8c55b515717939ae96f474469bc5747d91fc0
teruhisa.akiyoshi <lento.grazioso@gmail.com>
2021-08-03 14:12:31 +0000

    add omikuji function

--- sample.php
+++ sample.php
@@ -1,6 +1,14 @@
 <?php
 
     $result = "無し";
+    if (array_key_exists('act', $_POST)) {
+        $result = omikuji();
+    }
+    
+    function omikuji() {
+        $fortune = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
+        return $fortune[random_int(0, count($fortune) - 1)];
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +19,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：<?php print specialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
